3e53711f631270f6f0d2d54a755b6dd7fcbe2f08
Nic Schworer <nic.schworer@gmail.com>
2020-11-29 12:11:35 -0500

    initial commit

---
+++ Develop/models/workout.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now 
+    },
+    exercises: [
+        {
+        type: {
+            type: String,
+            required: true },
+        name: {
+            type: String,
+            required: true },
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutsSchema);
+
+module.exports = Workout;
