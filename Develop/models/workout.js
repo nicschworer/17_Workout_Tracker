8b7816386d197a200b46eda8a4d484220b7601f0
Nic Schworer <nic.schworer@gmail.com>
2020-11-29 16:33:06 -0500

    further routes)

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -24,6 +24,6 @@ const workoutSchema = new Schema({
     ]
 });
 
-const Workout = mongoose.model("Workout", workoutsSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
+const mongojs = require("mongojs");
+const Workout = require("./models/workout");
 
 
 
@@ -23,17 +25,85 @@ const db = mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cust
 //   console.log("Database Error:", error);
 // });
 
+// html routes
 app.get("/", (req, res) => {
   res.send(index.html);
 });
 
 app.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/exercise.html"))
-})
+});
 
 app.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/stats.html"))
+});
+
+// api routes
+
+app.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    })
 })
+
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+  console.log(body);
+  Workout.update({_id: mongojs.ObjectId(params.id)}, {$push: {exercise: body}})
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  })
+  console.log("im there");
+});
+
+app.get("/api/workouts", (req, res) => {
+  Workout.find({})
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  })
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+  .then(dbWorkouts => {
+    res.json(dbWorkouts);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  })
+});
+
+app.delete("/api/workouts", ({ body }, res) => {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("app is listening on port 3000");
