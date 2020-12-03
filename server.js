8589d7ea0fc58f67028e45276c98c8554f8e61d8
Nic Schworer <nic.schworer@gmail.com>
2020-12-02 19:23:18 -0500

    updated port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const collections = ["workouts"];
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  process.env.PORT || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
