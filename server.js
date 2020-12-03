a1fd4b59272e6c5dc3fe5640390096a31fde2dd9
Nic Schworer <nic.schworer@gmail.com>
2020-12-02 20:18:04 -0500

    update port

--- server.js
+++ server.js
@@ -115,6 +115,6 @@ app.delete("/api/workouts", ({ body }, res) => {
 
 
 
-app.listen(3000, () => {
-  console.log("app is listening on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is listening on " + PORT);
 })
